d4b37acfa7cbb44b246af6630f2cab62d44cdccf
TOSUKUi <am.businessmail.exp@gmail.com>
2018-09-19 21:55:38 +0900

    add line massaging util and use it. modified stop order logic

---
+++ src/lineUtil.js
@@ -0,0 +1,13 @@
+function lineNotify(accessToken, message){
+    var url = "https://notify-api.line.me/api/notify"
+    var payload = "message="  + message
+    var params = {
+        "method": "POST",
+        "headers": {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": 	"Bearer " + token
+        },
+        "payload": payload
+    }
+    UrlFetchApp.fetch(url, params)
+}
