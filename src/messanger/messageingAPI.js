879fdea756dfa5d34c6a4b0294ac076f77f5dcaf
TOSUKUi <am.businessmail.exp@gmail.com>
2018-10-08 23:53:27 +0900

    メッセージがからのときはリクエストを送らないようにした

--- src/messanger/messageingAPI.js
+++ src/messanger/messageingAPI.js
@@ -1,6 +1,10 @@
+// TODO: 
+// [] 1
+
 function chatMessage(message){
   var sheet = SpreadsheetApp.getActive().getSheetByName("メッセージ")
   var messageConfig = sheet.getRange(2, 1, 1, 2).getValues()
+  if(message == ""){return}
   if(messageConfig[0][1] == ""){
     sheet.getRange(2, 2).setValue("ここに値を入力して下さい");
     throw "メッセージの送信に失敗しました"
