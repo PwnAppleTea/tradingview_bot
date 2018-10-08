67fefe3f79ad97911ee58dcd8f8fcca4017435f9
TOSUKUi <am.businessmail.exp@gmail.com>
2018-10-08 23:52:37 +0900

    オーダー取得とオーダーキャンセルを実装

--- src/platform_apis/bitmex_api.js
+++ src/platform_apis/bitmex_api.js
@@ -6,14 +6,20 @@ var bitmex = function(api_key, api_secret){
 bitmex.prototype.marketOrder = function(symbol, side, orderQty){
   var path = "/api/v1/order";
   var params = {"symbol": symbol, "side": side, "orderQty": orderQty, "ordType": "Market"};
-  var order = this.sendRequest_(params, "POST", path, 0)
-  return order
+  return this.parse_order(this.sendRequest_(params, "POST", path, 0))
+  
+}
+
+bitmex.prototype.getOrders = function(symbol, orderIDs){
+  var path = "/api/v1/order";
+  var params = {"symbol": symbol, "orderID": orderIDs};
+  return this.parse_orders(this.sendRequest_(params, "GET", path, 0))
 }
 
 bitmex.prototype.marketCloseOrder = function(symbol){
   var path = "/api/v1/order/closePosition"
   var params = {"symbol": symbol}
-  return this.sendRequest_(params, "POST", path, 0)
+  return this.parse_order(this.sendRequest_(params, "POST", path, 0))
 }
 
 bitmex.prototype.getPosition = function(symbol){
@@ -23,6 +29,9 @@ bitmex.prototype.getPosition = function(symbol){
   return position
 }
 
+/**
+ * TODO: add cancel order 
+**/
 bitmex.prototype.marketStopOrder = function(symbol, side, pegOffsetValue, pegPriceType, orderQty, positionPrice){
   var path = "/api/v1/order"
   var params = {}
@@ -37,7 +46,7 @@ bitmex.prototype.marketStopOrder = function(symbol, side, pegOffsetValue, pegPri
   Logger.log(params)
   var position = this.sendRequest_(params, "POST", path, 0, 100000)
   Logger.log(position)
-  return position
+  return this.parse_order(position)
 }
 
 
@@ -60,7 +69,7 @@ bitmex.prototype.sendRequest_ = function(params, method, path, numResend){
   }
   if(method=="POST"){
     var payload = params
-    var signature = makeMexSignature(this.api_secret, method, nonce, path, payload);
+    var signature = this.makeMexSignature(this.api_secret, method, nonce, path, payload);
     option["headers"]["api-signature"] = signature
     option["payload"] = JSON.stringify(payload)
     var query = path
@@ -68,19 +77,19 @@ bitmex.prototype.sendRequest_ = function(params, method, path, numResend){
       var query = path
       query = path + "?filter=" + encodeURIComponent(JSON.stringify(params))
       var payload = ''
-      var signature = makeMexSignature(this.api_secret, method, nonce, query, payload)
+      var signature = this.makeMexSignature(this.api_secret, method, nonce, query, payload)
       option["headers"]["api-signature"] = signature
     }
 
   var response = UrlFetchApp.fetch(api_url+query, option)
-  if(checkHttpError(response) == "Resend"){
+  if(this.checkHttpError(response) == "Resend"){
     Utilities.sleep(500)
     return this.sendRequest_(params, method, path, numResend + 1)
   }
   return response
 }
 
-function checkHttpError(response){
+bitmex.prototype.checkHttpError = function(response){
   res = JSON.parse(response)
   code = response.getResponseCode()
   if(code >= 400){
@@ -92,20 +101,42 @@ function checkHttpError(response){
   }
 }
 
-function makeMexSignature(apiSecret, verb, expires, path, payload){
+bitmex.prototype.makeMexSignature = function(apiSecret, verb, expires, path, payload){
   if(payload == ""){
     var source = verb + path + expires
   }else{
     var s_payload = JSON.stringify(payload)
     var source = verb + path + expires + s_payload
   }
-  return hex(Utilities.computeHmacSha256Signature(source, apiSecret))    
+  return this.hex(Utilities.computeHmacSha256Signature(source, apiSecret))    
 }
 
-function hex(signature){
+bitmex.prototype.hex = function(signature){
   var sign = signature.reduce(function(str,chr){
     chr = (chr < 0 ? chr + 256 : chr).toString(16);
     return str + (chr.length==1?'0':'') + chr;
   },'');
   return sign
+}
+
+bitmex.prototype.parse_order = function(order){
+  var ordOrg = JSON.parse(order)
+  return {
+    "ticker": ordOrg["symbol"],
+    "side": ordOrg["side"],
+    "オーダーID": ordOrg["orderID"],
+    "ポジションサイズ": ordOrg["orderQty"],
+    "ストップ価格": ordOrg["stopPx"],
+    "執行価格": ordOrg["avgPx"],
+    "価格": ordOrg["price"],
+    "オーダーステータス": ordOrg["ordStatus"],
+    "執行時刻": ordOrg["transactTime"],
+    "timestamp": ordOrg["timestamp"],
+    "オーダータイプ": ordOrg["ordType"]
+  }
+}
+
+bitmex.prototype.parse_orders = function(order){
+  var ordOrgs = JSON.parse(order)
+  return ordOrgs.map(parse_order(ordOrg))
 }
--- src/platform_apis/bitmex_testnet_api.js
+++ src/platform_apis/bitmex_testnet_api.js
@@ -6,14 +6,19 @@ var bitmex_testnet = function(api_key, api_secret){
 bitmex_testnet.prototype.marketOrder = function(symbol, side, orderQty){
   var path = "/api/v1/order";
   var params = {"symbol": symbol, "side": side, "orderQty": orderQty, "ordType": "Market"};
-  var order = this.sendRequest_(params, "POST", path, 0)
-  return order
+  return this.parse_order(this.sendRequest_(params, "POST", path, 0))
 }
 
 bitmex_testnet.prototype.marketCloseOrder = function(symbol){
   var path = "/api/v1/order/closePosition"
   var params = {"symbol": symbol}
-  return this.sendRequest_(params, "POST", path, 0)
+  return this.parse_order(this.sendRequest_(params, "POST", path, 0))
+}
+
+bitmex_testnet.prototype.getOrders = function(symbol, orderIDs){
+  var path = "/api/v1/order";
+  var params = {"symbol": symbol, "orderID": orderIDs};
+  return this.parse_orders(this.sendRequest_(params, "GET", path, 0))
 }
 
 bitmex_testnet.prototype.getPosition = function(symbol){
@@ -37,7 +42,11 @@ bitmex_testnet.prototype.marketStopOrder = function(symbol, side, pegOffsetValue
   Logger.log(params)
   var position = this.sendRequest_(params, "POST", path, 0, 100000)
   Logger.log(position)
-  return position
+  return this.parse_order(position)
+}
+
+bitmex_testnet.prototype.cancelStopOrder = function(symbol, orderIDs){
+  //TODO: ストップロスをキャンセルするためのリクエスト
 }
 
 
@@ -80,7 +89,7 @@ bitmex_testnet.prototype.sendRequest_ = function(params, method, path, numResend
   return response
 }
 
-function checkHttpError(response){
+bitmex_testnet.prototype.checkHttpError = function(response){
   res = JSON.parse(response)
   code = response.getResponseCode()
   if(code >= 400){
@@ -92,20 +101,42 @@ function checkHttpError(response){
   }
 }
 
-function makeMexSignature(apiSecret, verb, expires, path, payload){
+bitmex_testnet.prototype.makeMexSignature = function(apiSecret, verb, expires, path, payload){
   if(payload == ""){
     var source = verb + path + expires
   }else{
     var s_payload = JSON.stringify(payload)
     var source = verb + path + expires + s_payload
   }
-  return hex(Utilities.computeHmacSha256Signature(source, apiSecret))    
+  return this.hex(Utilities.computeHmacSha256Signature(source, apiSecret))    
 }
 
-function hex(signature){
+bitmex_testnet.prototype.hex = function(signature){
   var sign = signature.reduce(function(str,chr){
     chr = (chr < 0 ? chr + 256 : chr).toString(16);
     return str + (chr.length==1?'0':'') + chr;
   },'');
   return sign
+}
+
+bitmex_testnet.prototype.parse_order = function(order){
+  var ordOrg = JSON.parse(order)
+  return {
+    "ticker": ordOrg["symbol"],
+    "side": ordOrg["side"],
+    "オーダーID": ordOrg["orderID"],
+    "ポジションサイズ": ordOrg["orderQty"],
+    "ストップ価格": ordOrg["stopPx"],
+    "執行価格": ordOrg["avgPx"],
+    "価格": ordOrg["price"],
+    "オーダーステータス": ordOrg["ordStatus"],
+    "執行時刻": ordOrg["transactTime"],
+    "timestamp": ordOrg["timestamp"],
+    "オーダータイプ": ordOrg["ordType"]
+  }
+}
+
+bitmex_testnet.prototype.parse_orders = function(order){
+  var ordOrgs = JSON.parse(order)
+  return ordOrgs.map(parse_order(ordOrg))
 }
